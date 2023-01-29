4a9facb02f0a13e9d0b88c699e5fd723a8639db5
Petchorine <christophe.couilleault@proton.me>
2023-01-29 10:38:07 +0100

    gestion v1 ok

--- gestion_liste.py
+++ gestion_liste.py
@@ -1,6 +1,6 @@
 import copy
 
-all_dico = [{'numero': '1', 'artiste': 'Frank Sinatra', 'titre': 'In the Wee Small Hours', 'annee': '1955'}, {'numero': '2', 'artiste': 'Elvis Presley', 'titre': 'Elvis Presley (album)', 'annee': '1956'}, {'numero': '3', 'artiste': 'The Louvin Brothers', 'titre': 'Tragic Songs of Life', 'annee': '1956'}, {'numero': '4', 'artiste': 'Louis Prima', 'titre': 'The Wildest!', 'annee': '1956'}, {'numero': '5', 'artiste': 'Fats Domino', 'titre': 'This is Fats', 'annee': '1956'}]
+all_dico = [{'numero': '1', 'artiste': 'Frank Sinatra', 'titre': 'In the Wee Small Hours', 'annee': '1955'}, {'numero': '2', 'artiste': 'Elvis Presley', 'titre': 'Elvis Presley (album)', 'annee': '1956'}, {'numero': '3', 'artiste': 'The Louvin Brothers', 'titre': 'Tragic Songs of Life', 'annee': '1956'}]
 
 
 remaining_list = copy.copy(all_dico)
@@ -9,62 +9,79 @@ unliked = []
 
 
 def add_to_liked_or_unliked():
-    print("Tape le n° de l'album :")
-    album_choice = input(">>> ")
-    print()
-    print("As-tu aimé cet album ?")
-    print(" 1. oui")
-    print(" 2. non")
-    user_choice = input(">>> ")
-
-    if user_choice == "1":       
-        for album in remaining_list:
-            if album["numero"] == album_choice:
-                liked.append(album_choice)
-                remaining_list.remove(album)
-                break
-
-    elif user_choice == "2":
-        unliked.append()
-        # del remaining_list
-    
-    while True:
-        print("Veux-tu tagger un nouvel album ?")
-        print(" 1. oui")
-        print(" 2. non")
+    while remaining_list:
+        print()
+        print("Que souhaites-tu faire ?")
+        print(" 1. tagger un nouvel album")
+        print(" 2. afficher les albums qu'il te reste à tagger")
+        print(" 3. revenir au menu")
         user_choice = input(">>> ")
 
         if user_choice == "1":
-            add_to_liked_or_unliked()
-        elif user_choice == "2":
-            break
-        else:
-            print("Tu dois choisir 1 ou 2.")
+            print()
+            print("Tape le n° de l'album :")
+            album_choice = input(">>> ")
+            
+            for album_present_or_not in remaining_list:
+                if album_choice in album_present_or_not.values():
+                    print()
+                    print("As-tu aimé cet album ?")
+                    print(" 1. oui")
+                    print(" 2. non")
+                    user_choice = input(">>> ")
 
+                    if user_choice == "1":       
+                        for album in remaining_list:
+                            if album["numero"] == album_choice:
+                                liked.append(album)
+                                remaining_list.remove(album)
+                                break
+                    elif user_choice == "2":
+                        for album in remaining_list:
+                            if album["numero"] == album_choice:
+                                unliked.append(album)
+                                remaining_list.remove(album)
+                                break
+                    else:
+                        print("Tu dois choisir 1 ou 2.")
+                        add_to_liked_or_unliked()
+                else:
+                    print("Cet album n'est pas dans la liste.")
+                    break
+        elif user_choice == "2":
+            show_remaining_list()
+        elif user_choice == "3":
+            print("menu")
+            return remaining_list, liked, unliked
+        else:
+            print("Tu dois choisir entre 1, 2 ou 3.")
+            continue
+
+    print("Tous les albums ont été taggés.")   
+
+def show_remaining_list():
+        print()
+        print("""
+n°   Artiste                        Album                          Annee
+==== ============================== ============================== =====
+""")
+        for toto in remaining_list:
+            print(toto["numero"].ljust(4), toto["artiste"][:30].ljust(30, "."), 
+                    toto["titre"][:27].ljust(30, "."), toto["annee"].rjust(4))
 
 def intro():
-    print("Que souhaites-tu faire ?")
-    print(" 1. tagger un nouvel album")
-    print(" 2. revenir au menu")
-    user_choice = input(">>> ")
-
-    if user_choice == "1":
-        print()
-        print("Titre")
-        
-        # TODO : afficher le dictionnaire
-
-        # add_to_liked_or_unliked()
-    else:
-        print("menu")
-
+    # TODO : menu choix quelle décénie
+    print()
+    print("Voici les albums qu'il te reste à tagger :")
+    show_remaining_list()
+    add_to_liked_or_unliked()
 
 intro()
 
 
 print()
 print("****************************************************************")
-print("all_dico       : ", len(all_dico), " - ", all_dico)
+print("all_dico       : ", len(all_dico))
 print("remaining_list : ", len(remaining_list), " - ", remaining_list )
-print("liked          : ", len(liked), " - ", liked)
+print("liked          : ", len(liked) , " - ", liked)
 print("unliked        : ", len(unliked), " - ", unliked )
