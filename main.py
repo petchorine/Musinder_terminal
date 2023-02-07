187dfb109747e1aca9f1e1dffe238f56ee09f362
Petchorine <christophe.couilleault@proton.me>
2023-02-07 09:18:54 +0100

    avant tests pour suppr listes afin de n'utiliser que la db

--- main.py
+++ main.py
@@ -98,13 +98,13 @@ class User:
             self.db_manager.execute_query(insert_query, (album_id, artist, title, year, decade))
             self.db_manager.connect().commit()
                 
-    def add_to_liked_or_unliked(self):
+    def add_to_liked_or_unliked(self, decade):
         while self.remaining_list:
             print()
             print("Que souhaites-tu faire ?")
             print(" 1. tagger un nouvel album")
-            print(" 2. afficher les albums qu'il te reste à tagger")
-            print(" 3. revenir au menu")
+            print(" 2. afficher la liste des albums encore à tagger")
+            print(" 3. revenir au menu et changer de décennie.")
             user_choice = input(">>> ")
 
             if user_choice == "1":
@@ -112,26 +112,29 @@ class User:
                 print("Tape le n° de l'album :")
                 album_choice = input(">>> ")
                 
-                for album_present_or_not in self.remaining_list:
-                    if album_choice in album_present_or_not.values():
-                        print()
-                        print("As-tu aimé cet album ?")
-                        print(" 1. oui")
-                        print(" 2. non")
-                        user_choice = input(">>> ")
 
-                        if user_choice == "1":       
-                            self.liked_album(user_choice)
-                        elif user_choice == "2":
-                            self.unliked_album(user_choice)
-                        else:
-                            print("Tu dois choisir 1 ou 2.")
-                            self.add_to_liked_or_unliked()
-                    else:
-                        print("Cet album n'est pas dans la liste.")
+                album = self.db_manager.execute_query(f'''SELECT * FROM remaining_list WHERE album_id = {album_choice}''')
+                if album:
+                    print()
+                    print(f"As-tu aimé l'album '{album[0][2]}' de {album[0][1]} ?")
+                    print(" 1. oui")
+                    print(" 2. non")
+                    user_choice = input(">>> ")
+
+                    if user_choice == "1":       
+                        self.liked_album(user_choice)
                         break
+                    elif user_choice == "2":
+                        self.unliked_album(user_choice)
+                        break
+                    else:
+                        print("Tu dois choisir 1 ou 2.")
+                        self.add_to_liked_or_unliked()
+                else:
+                    print("Cet album n'est pas dans la liste.")
+                    break
             elif user_choice == "2":
-                self.show_remaining_list()
+                self.show_remaining_list(decade)
             elif user_choice == "3":
                 print("menu")
                 return self.remaining_list, self.liked_list, self.unliked_list
@@ -139,31 +142,25 @@ class User:
                 print("Tu dois choisir entre 1, 2 ou 3.")
                 continue
 
-        print("Tous les albums ont été taggés.") 
+        if self.remaining_list is None:
+            print("Tous les albums ont été taggés.") 
 
     def show_remaining_list(self, choice):
-        choices = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020]    
         print()
+        print(f"Voici les albums qu'il te reste à tagger pour les années {choice}.")
         print("""
 n°   Artiste                        Album                          Annee
 ==== ============================== ============================== =====
 """)
                 
-        rows = self.db_manager.execute_query(f'''SELECT * FROM remaining_list WHERE decade = {choices[choice - 1]}''')
+        rows = self.db_manager.execute_query(f'''SELECT * FROM remaining_list WHERE decade = {choice}''')
         for row in rows:
             print(row[0].ljust(4), row[1][:30].ljust(30, "."), 
                     row[2][:27].ljust(30, "."), row[3].rjust(4))
 
-    def liked_album(self, album_id):
-        flag = False
-        for album in self.remaining_list:
-            if album.album_id == album_id:
-                self.liked_list.append(album)
-                self.remaining_list.remove(album)
-                flag = True
 
-            if flag:
-                self.db_manager.execute_query(f"SELECT * FROM liked_list WHERE album_id='{album_id}'")
+    def liked_album(self, album_id):
+            if self.db_manager.execute_query(f"SELECT * FROM liked_list WHERE album_id='{album_id}'"):
                 
                 with sqlite3.connect(fr'C:\Users\chris\Desktop\monPyhon\mes_projets_python\musinder\V0\{self.username}_albums.db') as connection:
                     cursor = connection.cursor()
@@ -172,12 +169,39 @@ n°   Artiste                        Album                          Annee
                     self.db_manager.execute_query('''INSERT INTO liked_list VALUES (?,?,?,?,?)''', (album_id, album.artist, album.title, album.year, album.decade))
                     self.db_manager.execute_query('''DELETE FROM remaining_list WHERE album_id=?''', (album_id,))
                     self.db_manager.connect().commit()
-                    print(f"L'album a été ajouté à liked_list")
-                    break
+                    print(f"L'album a été ajouté à liked_list")                
                 else:
                     print(f"L'album existe déjà dans liked_list")
             else:
                 print("L'album n'a pas été trouvé dans la remaining_list")
+
+
+
+
+    # def liked_album(self, album_id):
+    #     flag = False
+    #     for album in self.remaining_list:
+    #         if album.album_id == album_id:
+    #             self.liked_list.append(album)
+    #             self.remaining_list.remove(album)
+    #             flag = True
+
+    #         if flag:
+    #             self.db_manager.execute_query(f"SELECT * FROM liked_list WHERE album_id='{album_id}'")
+                
+    #             with sqlite3.connect(fr'C:\Users\chris\Desktop\monPyhon\mes_projets_python\musinder\V0\{self.username}_albums.db') as connection:
+    #                 cursor = connection.cursor()
+
+    #             if not cursor.fetchone():
+    #                 self.db_manager.execute_query('''INSERT INTO liked_list VALUES (?,?,?,?,?)''', (album_id, album.artist, album.title, album.year, album.decade))
+    #                 self.db_manager.execute_query('''DELETE FROM remaining_list WHERE album_id=?''', (album_id,))
+    #                 self.db_manager.connect().commit()
+    #                 print(f"L'album a été ajouté à liked_list")
+    #                 break
+    #             else:
+    #                 print(f"L'album existe déjà dans liked_list")
+    #         else:
+    #             print("L'album n'a pas été trouvé dans la remaining_list")
                 
     def unliked_album(self, album_id):
         flag = False
@@ -206,6 +230,10 @@ n°   Artiste                        Album                          Annee
     def close(self):
         self.conn.close
 
+for i in range(len(get_albums(URL_WIKI)[0])):
+    BASE = get_albums(URL_WIKI)[0][i]
+    toto = User("toto", BASE)
+
 def introduction():
     nbr_albums = get_albums(URL_WIKI)[1]
     print("""
@@ -223,35 +251,39 @@ Tu es sur la version Terminal de l'application Musinder.
 Tu vas pouvoir tagger les albums que tu aimes bien dans la liste des "1001 albums qu'il faut avoir écouté dans sa vie". 
 Cette liste a été publiée à partir de 2006 sous la direction de Robert Dimery.
 Elle contient actuellement {nbr_albums} albums.
-
-Les albums de quelle décennie souhaites-tu voir ?
-    1. 1950
-    2. 1960
-    3. 1970
-    4. 1980
-    5. 1990
-    6. 2000
-    7. 2010
-    8. 2020
 """)
 
-
-for i in range(len(get_albums(URL_WIKI)[0])):
-    BASE = get_albums(URL_WIKI)[0][i]
-    toto = User("toto", BASE)
-
+def decade_choice():
+    choices = [1950, 1960, 1970, 1980, 1990, 2000, 2010, 2020]
+    print()
+    print("Les albums de quelle décennie souhaites-tu voir ?")
+    print(" 1. 1950")
+    print(" 2. 1960")
+    print(" 3. 1970")
+    print(" 4. 1980")
+    print(" 5. 1990")
+    print(" 6. 2000")
+    print(" 7. 2010")
+    print(" 8. 2020")
+    choice_decade = input(">>> ")
+    choice_decade_int = int(choice_decade)
+    toto.show_remaining_list(choices[choice_decade_int - 1])
+    return choices[choice_decade_int - 1]
 
 introduction()
+decade = decade_choice()
+toto.add_to_liked_or_unliked(decade)
 
-# permet d'afficher les albums par décenie
-# while True:
-#     choice_decade = input(">>> ")
-#     if choice_decade == "q":
-#         break
-#     else:
-#         choice_decade_int = int(choice_decade)
-#         toto.show_remaining_list(choice_decade_int)
+
+
+
+
+
+
+
+
+
 
 
 # ajoute l'album à liked_list
-toto.liked_album("1083")
+# toto.liked_album("1083")
